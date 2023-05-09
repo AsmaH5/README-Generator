3c4e6e522b0b41f5028ae28f7f5da4e1dd2bbe1b
Asma Hassan <asmasheikhhassan@gmail.com>
2023-05-09 14:42:16 +0100

    Added readme questions for user

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# README-Generator-2
+# asfdd
+
+## Description
+
+safdf
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+sdfd
+
+## Usage
+
+dfd
+
+## License
+
+This project is licensed under the undefined license.
+
+## Contributing
+
+undefined
+
+## Tests
+
+dfd
+
+## Questions
+
+If you have any questions about this project, please contact me at dfd. You can find more of my work at https://github.com/df.
--- index.js
+++ index.js
@@ -1,11 +1,61 @@
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
+
+// array of questions for user using inquirer
 const questions = [
-
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is your project title?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'What is your project about?',
+    },
+    {
+      type: 'input',
+      name: 'Table of Content',
+      message: 'Press enter if you would like a Table of Content',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What would a user need to install this app?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'How is this app used?',
+    },
+    {
+      type: 'list',
+      name: 'License',
+      message: 'What license will you be using?',
+      choices: ['MIT', 'GPLv2','Apache', 'Other', 'None']
+    },
+    {
+      type: 'input',
+      name: 'Contributing',
+      message: 'Who has contributed to this project?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'What tests are required to run your project?',
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'What is your Github username?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+    },
 ];
 
 // function to write README file
